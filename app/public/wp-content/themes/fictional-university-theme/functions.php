641139482e945aeefad61b1ee2837e31435dbebe
SHjNEe <baotrung.jpvn@gmail.com>
2023-10-02 23:01:41 +0700

    Add

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -33,19 +33,36 @@ function pageBanner($args = NULL)
 
 function university_files()
 {
-  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDekJTa_uCZuN8RDdAtLVW31eNkC3G0a1s', NULL, '1.0', true);
+  // Enqueue Bootstrap CSS from CDN
+  wp_enqueue_style('bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+
+  // Enqueue Bootstrap JS from CDN
+  wp_enqueue_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), '4.5.2', true);
+
+  // Enqueue Google Maps API
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=YOUR_API_KEY', NULL, '1.0', true);
+
+  // Enqueue your main JavaScript file
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+
+  // Enqueue custom Google Fonts
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+  // Enqueue Font Awesome icons
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+  // Enqueue your main stylesheets
   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 
+  // Localize data for your JavaScript file
   wp_localize_script('main-university-js', 'universityData', array(
     'root_url' => get_site_url()
   ));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
 
 function university_features()
 {
